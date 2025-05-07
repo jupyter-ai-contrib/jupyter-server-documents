9e746678bb3aef3620601266d25e400a64f664af
David L. Qiu <david@qiu.dev>
2025-05-07 13:55:42 -0700

    add double handshake & YDoc observer

--- jupyter_rtc_core/rooms/yroom.py
+++ jupyter_rtc_core/rooms/yroom.py
@@ -31,8 +31,13 @@ class YRoom:
         self._client_group = YjsClientGroup()
         self._message_queue = asyncio.Queue()
 
-        # Start background task that handles messages in the message queue
+        # Start background task that routes new messages in the message queue
+        # to the appropriate handler method.
         self.loop.create_task(self._on_new_message())
+
+        # Start observer on the `ydoc` to ensure new updates are broadcast to
+        # all clients and saved to disk.
+        self.ydoc.observe(lambda event: self.write_sync_update(event.update))
     
 
     @property
@@ -115,8 +120,11 @@ class YRoom:
 
     def handle_sync_step1(self, client_id: str, message: bytes) -> None:
         """
-        Handles SyncStep1 messages from new clients by computing & replying with
-        a SyncStep2 message.
+        Handles SyncStep1 messages from new clients by:
+
+        - Computing a SyncStep2 reply,
+        - Sending the reply to the client over WS, and
+        - Sending a new SyncStep1 message immediately after.
         """
         # Mark client as desynced
         new_client = self.clients.get(client_id, synced_only=False)
@@ -136,6 +144,7 @@ class YRoom:
             return
 
         # Write SyncStep2 reply to the client's WebSocket
+        # Marks client as synced.
         try:
             # TODO: remove the assert once websocket is made required
             assert isinstance(new_client.websocket, WebSocketHandler)
@@ -148,13 +157,48 @@ class YRoom:
             )
             self.log.exception(e)
             return
+        
+        # Send SyncStep1 message
+        try:
+            assert isinstance(new_client.websocket, WebSocketHandler)
+            sync_step1_message = pycrdt.create_sync_message(self.ydoc)
+            new_client.websocket.write_message(sync_step1_message)
+        except Exception as e:
+            self.log.error(
+                "An exception occurred when writing a SyncStep1 message "
+                f"to newly-synced client '{new_client.id}':"
+            )
+            self.log.exception(e)
+
+
+    def handle_sync_step2(self, client_id: str, message: bytes) -> None:
+        """
+        Handles SyncStep2 messages from newly-synced clients by applying the
+        SyncStep2 message to YDoc.
+
+        A SyncUpdate message will automatically be broadcast to all synced
+        clients after this method is called via the `self.write_sync_update()`
+        observer.
+        """
+        try:
+            message_payload = message[1:]
+            pycrdt.handle_sync_message(message_payload, self.ydoc)
+        except Exception as e:
+            self.log.error(
+                "An exception occurred when applying a SyncStep2 message "
+                f"from client '{client_id}':"
+            )
+            self.log.exception(e)
+            return
 
 
     def handle_sync_update(self, client_id: str, message: bytes) -> None:
         """
-        Handles SyncUpdate messages from new clients by applying the update to
-        the YDoc, broadcasting the update to all other clients, and saving the
-        YDoc to disk.
+        Handles incoming SyncUpdate messages by applying the update to the YDoc.
+
+        A SyncUpdate message will automatically be broadcast to all synced
+        clients after this method is called via the `self.write_sync_update()`
+        observer.
         """
         # Ignore the message if client is desynced
         if self._should_ignore_update(client_id, "SyncUpdate"):
@@ -172,10 +216,27 @@ class YRoom:
             self.log.exception(e)
             return
         
-        # Broadcast the SyncUpdate to all other synced clients
-        self._broadcast_message_from(client_id, message, message_type="SyncUpdate")
-        
-        # Finally, save the file to disk
+        # Broadcast the SyncUpdate to all other synced clients and save the YDoc
+        # to disk.
+        self.write_sync_update(message_payload, client_id)
+
+
+    def write_sync_update(self, message_payload: bytes, client_id: str | None = None) -> None:
+        """
+        This method is an observer on `self.ydoc` which:
+
+        - Broadcasts a SyncUpdate message payload to all connected clients by
+        writing to their respective WebSockets,
+
+        - Persists the contents of the updated YDoc by writing to disk.
+
+        This method can also be called manually.
+        """
+        # Broadcast the message:
+        message = pycrdt.create_update_message(message_payload)
+        self._broadcast_message(message, message_type="SyncUpdate")
+
+        # Save the file to disk.
         # TODO: requires YRoomLoader implementation
         return
 
@@ -198,7 +259,7 @@ class YRoom:
             return
 
         # Broadcast AwarenessUpdate message to all other synced clients
-        self._broadcast_message_from(client_id, message, message_type="AwarenessUpdate")
+        self._broadcast_message(message, message_type="AwarenessUpdate")
 
 
     def _should_ignore_update(self, client_id: str, message_type: Literal['AwarenessUpdate', 'SyncUpdate']) -> bool:
@@ -220,23 +281,23 @@ class YRoom:
         return False
     
 
-    def _broadcast_message_from(self, client_id: str, message: bytes, message_type: Literal['AwarenessUpdate', 'SyncUpdate']):
+    def _broadcast_message(self, message: bytes, message_type: Literal['AwarenessUpdate', 'SyncUpdate']):
         """
         Broadcasts a given message from a given client to all other clients.
         This method automatically logs warnings when writing to a WebSocket
         fails. `message_type` is used to produce more readable warnings.
         """
-        other_clients = self.clients.get_others(client_id)
-        for other_client in other_clients:
+        clients = self.clients.get_all()
+        for client in clients:
             try:
                 # TODO: remove this assertion once websocket is made required
-                assert isinstance(other_client.websocket, WebSocketHandler)
-                other_client.websocket.write_message(message)
+                assert isinstance(client.websocket, WebSocketHandler)
+                client.websocket.write_message(message)
             except Exception as e:
                 self.log.warning(
                     f"An exception occurred when broadcasting a "
-                    f"{message_type} message from client '{client_id}' "
-                    f"to client '{other_client.id}:'"
+                    f"{message_type} message "
+                    f"to client '{client.id}:'"
                 )
                 self.log.exception(e)
 
@@ -244,3 +305,4 @@ class YRoom:
     def stop(self) -> None:
         # TODO: requires YRoomLoader implementation
         return
+
