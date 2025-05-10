f691aa488f0247d7c58f15a5d727fb402e0756dd
Zach Sailer <zsailer@apple.com>
2025-05-09 17:05:31 -0700

    [WIP] Provide a YDoc-aware session manager and kernel client.

--- jupyter_rtc_core/app.py
+++ jupyter_rtc_core/app.py
@@ -1,8 +1,10 @@
 from jupyter_server.extension.application import ExtensionApp
 from traitlets.config import Config
-
+from traitlets import Instance
+from traitlets import Type
 from .handlers import RouteHandler
 from .websockets import GlobalAwarenessWebsocket, YRoomWebsocket
+from .rooms.yroom_manager import YRoomManager
 
 class RtcExtensionApp(ExtensionApp):
     name = "jupyter_rtc_core"
@@ -19,9 +21,24 @@ class RtcExtensionApp(ExtensionApp):
         # (r"api/collaboration/room/(.*)", YRoomWebsocket)
     ]
 
+    yroom_manager_class = Type(
+        klass=YRoomManager,
+        help="""YRoom Manager Class.""",
+        default_value=YRoomManager,
+    ).tag(config=True)
+
+    yroom_manager = Instance(
+        klass=YRoomManager,
+        help="An instance of the YRoom Manager.",
+        allow_none=True
+    ).tag(config=True)
+
+    def initialize_settings(self):
+        self.yroom_manager = self.yroom_manager_class(parent=self)
+        self.settings.update({"yroom_manager": self.yroom_manager})
+
     def initialize(self):
         super().initialize()
-    
 
     def _link_jupyter_server_extension(self, server_app):
         """Setup custom config needed by this extension."""
@@ -29,5 +46,6 @@ class RtcExtensionApp(ExtensionApp):
         c.ServerApp.kernel_websocket_connection_class = "jupyter_rtc_core.kernels.websocket_connection.NextGenKernelWebsocketConnection"
         c.ServerApp.kernel_manager_class = "jupyter_rtc_core.kernels.multi_kernel_manager.NextGenMappingKernelManager"
         c.MultiKernelManager.kernel_manager_class = "jupyter_rtc_core.kernels.kernel_manager.NextGenKernelManager"
+        c.ServerApp.session_manager_class = "jupyter_rtc_core.session_manager.YDocSessionManager"
         server_app.update_config(c)
         super()._link_jupyter_server_extension(server_app)
--- jupyter_rtc_core/kernels/websocket_connection.py
+++ jupyter_rtc_core/kernels/websocket_connection.py
@@ -1,22 +1,8 @@
-import asyncio
-import json
-
-from traitlets import Bool
-from traitlets import default
-from traitlets import Instance
-
-try:
-    from jupyter_client.jsonutil import json_default
-except ImportError:
-    from jupyter_client.jsonutil import date_default as json_default
-
 from tornado.websocket import WebSocketClosedError
 from jupyter_server.services.kernels.connection.base import (
     BaseKernelWebsocketConnection,
 )
-from .states import LIFECYCLE_DEAD_STATES
 from jupyter_server.services.kernels.connection.base import deserialize_msg_from_ws_v1, serialize_msg_to_ws_v1
-from jupyter_client.session import Session
 
 class NextGenKernelWebsocketConnection(BaseKernelWebsocketConnection):
     """A websocket client that connects to a kernel manager.
@@ -36,23 +22,19 @@ class NextGenKernelWebsocketConnection(BaseKernelWebsocketConnection):
         self.kernel_manager.broadcast_state()
         self.log.info("Kernel websocket is now listening to kernel.")
 
-    def disconnect(self):
+    async def disconnect(self):
         self.kernel_manager.main_client.remove_listener(self.handle_outgoing_message)
 
-    def handle_incoming_message(self, ws_message):
+    def handle_incoming_message(self, incoming_msg):
         """Handle the incoming WS message"""
-        channel_name, msg_list = deserialize_msg_from_ws_v1(ws_message)
+        channel_name, msg_list = deserialize_msg_from_ws_v1(incoming_msg)
         if self.kernel_manager.main_client:
-            self.kernel_manager.main_client.send_message(channel_name, msg_list)
+            self.kernel_manager.main_client.handle_incoming_message(channel_name, msg_list)
 
     def handle_outgoing_message(self, channel_name, msg):
         """Handle the ZMQ message."""
         try:
-            # Taken from https://github.com/jupyter-server/jupyter_server/blob/e12feb99bb6171f2780610bbac6ba0f80aaec544/jupyter_server/services/kernels/connection/channels.py#L490
-            # Websocket protocol is complicated.
-            _, fed_msg_list = self.kernel_manager.main_client.session.feed_identities(msg)
-            parts = fed_msg_list[1:]
-            msg = serialize_msg_to_ws_v1(parts, channel_name)
+            msg = serialize_msg_to_ws_v1(msg, channel_name)
             self.websocket_handler.write_message(msg, binary=True)
         except WebSocketClosedError:
             self.log.warning("A ZMQ message arrived on a closed websocket channel.")
