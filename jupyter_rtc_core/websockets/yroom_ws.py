55f03f028f27c4c8e4da4268930857b5a614b39f
David L. Qiu <david@qiu.dev>
2025-05-12 14:30:11 -0700

    fix add_message() call and impl prepare()

--- jupyter_rtc_core/websockets/yroom_ws.py
+++ jupyter_rtc_core/websockets/yroom_ws.py
@@ -1,34 +1,53 @@
-"""
-WIP.
-
-This file just contains an interface to be filled out later.
-"""
-
 from tornado.httpclient import HTTPError
 from tornado.websocket import WebSocketHandler
 from ..rooms import YRoom, YRoomManager
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from jupyter_server_fileid.manager import BaseFileIdManager
 
 class YRoomWebsocket(WebSocketHandler):
     yroom: YRoom
+    room_id: str
     client_id: str
 
     @property
     def yroom_manager(self) -> YRoomManager:
         return self.settings["yroom_manager"]
+    
+
+    @property
+    def fileid_manager(self) -> BaseFileIdManager:
+        return self.settings["file_id_manager"]
+
+
+    def prepare(self):
+        # Bind `room_id` attribute
+        request_path: str = self.request.path
+        self.room_id = request_path.strip("/").split("/")[-1]
+
+        # Verify the file ID contained in the room ID points to a valid file.
+        fileid = self.room_id.split(":")[-1]
+        path = self.fileid_manager.get_path(fileid)
+        if not path:
+            raise HTTPError(404, f"No file with ID '{fileid}'.")
+    
 
     def open(self):
-        request_path: str = self.request.path
-        room_id = request_path.strip("/").split("/")[-1]
-        yroom = self.yroom_manager.get_room(room_id)
-
+        # Create the YRoom
+        yroom = self.yroom_manager.get_room(self.room_id)
         if not yroom:
-            raise HTTPError(404, f"No room with ID '{room_id}'.")
-
+            raise HTTPError(500, f"Unable to initialize YRoom '{self.room_id}'.")
         self.yroom = yroom
+
+        # Add self as a client to the YRoom
         self.client_id = self.yroom.clients.add(self)
 
+
     def on_message(self, message: bytes):
-        self.yroom.add_message(message)
+        # Route all messages to the YRoom for processing
+        self.yroom.add_message(self.client_id, message)
+
 
     def on_close(self):
         self.yroom.clients.remove(self.client_id)
