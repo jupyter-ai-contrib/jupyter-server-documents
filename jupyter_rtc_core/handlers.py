e92faafa5df182111e9122ba05cf83f19b557d1a
Zachary Sailer <zachsailer@gmail.com>
2025-05-14 20:57:32 -0700

    Use the real path in session creation to start a yroom (#49)
    * Use the real path in session creation to start a yroom
    
    * Handle no path or name

--- jupyter_rtc_core/handlers.py
+++ jupyter_rtc_core/handlers.py
@@ -42,35 +42,3 @@ class RouteHandler(APIHandler):
         self.finish(json.dumps({
             "data": "This is /jupyter-rtc-core/get-example endpoint!"
         }))
-
-
-# TODO: remove this by v1.0.0 if deemed unnecessary. Just adding this for
-# compatibility with the `jupyter_collaboration` frontend.
-class YRoomSessionHandler(APIHandler):
-    SESSION_ID = str(uuid.uuid4())
-
-    @tornado.web.authenticated
-    def put(self, path):
-        body = json.loads(self.request.body)
-        format = body["format"]
-        content_type = body["type"]
-        # self.log.info("IN HANDLER")
-        # for k, v in self.settings.items():
-        #     print(f"{k}: {v}")
-        # print(len(self.settings.items()))
-        # print(id(self.settings))
-
-        file_id_manager = self.settings["file_id_manager"]
-        file_id = file_id_manager.index(path)
-
-        data = json.dumps(
-            {
-                "format": format,
-                "type": content_type,
-                "fileId": file_id,
-                "sessionId": self.SESSION_ID,
-            }
-        )
-        self.set_status(200)
-        self.finish(data)
-
