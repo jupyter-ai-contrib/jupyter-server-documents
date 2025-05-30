c922f8eba676ff21176cee5d49ea3333a29472d4
Piyush Jain <piyushjain@duck.com>
2025-05-29 23:53:26 -0700

    Updated handler to accommodate all outputs

--- jupyter_server_documents/outputs/handlers.py
+++ jupyter_server_documents/outputs/handlers.py
@@ -19,6 +19,7 @@ class OutputsAPIHandler(APIHandler):
     @web.authenticated
     @authorized
     async def get(self, file_id=None, cell_id=None, output_index=None):
+        print("Reached the handler...")
         try:
             if output_index:
                 output = self.outputs.get_output(file_id, cell_id, output_index)
