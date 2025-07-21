c8e91cf099f0caf670dc0b5ffe9d18db0a275daa
Zach Sailer <zsailer@apple.com>
2025-07-21 11:42:48 -0700

    Fix kernel restart

--- jupyter_server_documents/kernels/multi_kernel_manager.py
+++ jupyter_server_documents/kernels/multi_kernel_manager.py
@@ -8,3 +8,10 @@ class NextGenMappingKernelManager(AsyncMappingKernelManager):
     
     def stop_buffering(self, kernel_id):
         pass
+
+    # NOTE: Since we disable watching activity and buffering here, 
+    # this method needs to be forked and remove code related to these things. 
+    async def restart_kernel(self, kernel_id, now=False):
+        """Restart a kernel by kernel_id"""
+        self._check_kernel_id(kernel_id)
+        await self.pinned_superclass._async_restart_kernel(self, kernel_id, now=now)
